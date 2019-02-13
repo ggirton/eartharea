6761cb394905d58dd011df2b5795d9ebdac9dba1
George Girton <george.girton@gmail.com>
2019-02-12 16:44:44 -0800

    Updated print statement for python3

--- earthArea.py
+++ earthArea.py
@@ -1,13 +1,18 @@
 from decimal import *
 
 PI= Decimal('3.1415926535897931')
-RADIUS= Decimal('6372795.477598')
+RADIUS= Decimal('6372795.477598')	#Earth radius in meters
 RADIUS1 = RADIUS+1
+
+# Formula for volume is four-thirds pi R cubed
 
 m1 = 4 * PI / 3
 V= m1 * RADIUS * RADIUS * RADIUS
 V1= m1 * RADIUS1 * RADIUS1 * RADIUS1
 
-print V ," cubic meters"
+print (V ," cubic meters")
 area = V1-V
-print area , " square meters"
+print (area , " square meters")
+quadrillion = 1000 * 1000 * 1000 * 1000 
+print(quadrillion)
+print (area/quadrillion, "quadrillion square meters")
